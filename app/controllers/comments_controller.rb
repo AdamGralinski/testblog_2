5aa3b34fd6bba3ea694fcbd040bd601a8926b28b
metraton1 <metraton1991@gmail.com>
2013-10-08 20:52:44 +0200

    multi-vote prevention

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
-  def create
-    post = Post.find(params[:post_id])
-    comment = post.comments.create(params[:comment])
-    comment.commenter = User.nickname
-    comment.voteup_count = 0
-    comment.votedown_count = 0
-    comment.users_vote = Array.new
-    comment.user_id = current_user.id
-    comment.save
-    redirect_to post_path(post)
-  end
+    def create
+      post = Post.find(params[:post_id])
+      comment = post.comments.create(params[:comment])
+      comment.commenter = User.name
+      comment.voteup_count = 0
+      comment.votedown_count = 0
+      comment.users_vote = Array.new
+      comment.user_id = current_user.id
+      comment.save
+      redirect_to post_path(post)
+    end
 
-  def destroy
+    def destroy
       post = Post.find(params[:id])
       comment = post.comments.find(params[:post_id])
       comment.destroy
@@ -21,18 +21,28 @@ class CommentsController < ApplicationController
     def voteup
       post = Post.find(params[:id])
       comment = post.comments.find(params[:post_id])
-      comment.voteup_count += 1
-      comment.save
-      redirect_to post_path(post)
+      if !comment.users_vote.include?(current_user.id) 
+        comment.users_vote << current_user.id
+        comment.voteup_count += 1
+        comment.save
+        redirect_to post_path(post)
+      else
+        redirect_to post_path(post)
+      end
     end
 
     def votedown
       post = Post.find(params[:id])
       comment = post.comments.find(params[:post_id])
-      comment.votedown_count += 1
-      try_abusive()
-      comment.save
-      redirect_to post_path(post)
+      if !comment.users_vote.include?(current_user.id) 
+        comment.users_vote << current_user.id
+        comment.votedown_count += 1
+        try_abusive()
+        comment.save
+        redirect_to post_path(post)
+      else
+        redirect_to post_path(post)
+      end
     end
 
     def try_abusive
