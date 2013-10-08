09ce79f8f0133ae62930ec4024007e63f2db888b
metraton1 <metraton1991@gmail.com>
2013-10-08 19:20:10 +0200

    comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment
+  include Mongoid::Document
+  field :commenter, type: String
+  field :body, type: String
+  field :voteup_count, type: Integer
+  field :votedown_count, type: Integer
+  field :abusive, type: Boolean, default: false
+  field :user_id, type: String
+  embedded_in :post
 end
